af7b36eea8881fa8d93b1d96afef584747f91ba8
undoZen <opensource@undozen.com>
2015-03-08 23:44:01 +0800

    save to redis

--- api.js
+++ api.js
@@ -5,15 +5,18 @@ var co = require('co');
 var sortBy = require('lodash').sortBy;
 var pluck = require('lodash').pluck;
 var config = require('config');
+var redis = require('redis');
+Promise.promisifyAll(require("redis"));
+var db = redis.createClient(config.redis_port || 6379, config.redis_host || '127.0.0.1');
 
 function requestOnce(f) {
     return function (p1) {
-        if (!p1) {
+        if (p1) {
             if (!f.cache) f.cache = {};
             if (f.cache[p1]) {
                 return f.cache[p1];
             } else {
-                f.cache[p1] = f();
+                f.cache[p1] = f(p1);
                 f.cache[p1].finally(function () {
                     delete f.cache[p1];
                 });
@@ -33,6 +36,9 @@ function requestOnce(f) {
     };
 }
 
+function ts(timestr) {
+    return (new Date(timestr)).valueOf();
+}
 var getAllProjects = requestOnce(co.wrap(function * () {
     console.time('ap');
     var body;
@@ -47,28 +53,49 @@ var getAllProjects = requestOnce(co.wrap(function * () {
             .end()).body;
         result = result.concat(body);
     } while (body.length);
-    console.time('ap');
+    var project;
+    for (i = -1; project = body[++i]; ) {
+	yield db.zaddAsync('projects', ~~(ts(project.last_activity_at) / 1000), project.id);
+	yield db.setAsync('project_detail:' + project.id, JSON.stringify(project));
+    }
+    console.timeEnd('ap');
     return result;
 }));
 getAllProjects();
 
-var getOpenedMergeRequests = co.wrap(function * (pid) {
+var getOpenedMergeRequests = requestOnce(co.wrap(function * (pid) {
     return (yield superagent.get(
             config.inner_url_prefix + '/api/v3/projects/' + pid +
             '/merge_requests?state=opened')
         .set('PRIVATE-TOKEN', 'PsfVVHfxMxya3YzM9HR5')
         .end()).body;
-});
+}));
 var getAllOpenedMergeRequests = co.wrap(function * () {
     console.time('aomr');
     var projects = yield getAllProjects();
+    var mrs;
     var result = [];
     for (var project, i = -1; project = projects[++i];) {
-        result = result.concat(yield getOpenedMergeRequests(project.id));
+	mrs = yield getOpenedMergeRequests(project.id);
+        if (mrs.length) {
+            result = result.concat(mrs);
+            yield db.zincrbyAsync('projects', mrs.length * 100000000, project.id);
+        }
     }
+    yield db.setAsync('omr', JSON.stringify(result));
     console.timeEnd('aomr');
     return result;
 });
+var getOpenedMergeRequestsByProjects = requestOnce(co.wrap(function * () {
+    var mrs = yield getAllOpenedMergeRequests();
+console.log('mrs', mrs);
+    var result = {};
+    mrs.forEach(function (mr) {
+	if (!result[mr.project_id]) result[mr.project_id] = [];
+        result[mr.project_id].push(mr);
+    });
+    return result;
+}));
 getAllProjects()
     .then(function (result) {
         //console.log(result);
@@ -76,5 +103,6 @@ getAllProjects()
         console.log('r', pluck(sortBy(result, 'last_activity_at').reverse(),
             'name_with_namespace'))
     })
-getAllOpenedMergeRequests()
-    .then(console.log.bind(console));
+getOpenedMergeRequestsByProjects()
+    .then(console.log.bind(console))
+    .catch(console.error.bind(console));
