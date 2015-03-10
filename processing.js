60f044b43cf847ada8c30aec4f47479e80eaac95
undoZen <opensource@undozen.com>
2015-03-10 16:04:07 +0800

    update processing.js

--- processing.js
+++ processing.js
@@ -7,6 +7,7 @@ var sortBy = require('lodash').sortBy;
 var pluck = require('lodash').pluck;
 var indexBy = require('lodash').indexBy;
 var transform = require('lodash').transform;
+var assign = require('lodash').assign;
 var config = require('config');
 var redis = require('redis');
 Promise.promisifyAll(require("redis"));
@@ -34,33 +35,75 @@ function countYeas(gms, ps, mr, notes, all) {
 		}
 	})).length;
 }
-function getReviews(gms, ps, mr) {
+function augmentNotes(gms, ps, mr) {
 	var gid = ps[mr.project_id].namespace.id;
 	log.trace({ type:'mr', gid: gid});
-	return mr.notes.reduce(function (result, note) {
-		log.trace({ type:'mr' }, note.author.id === mr.author.id)
-		log.trace({ type:'mr' }, !gms[gid][note.author.id])
-		log.trace({ type:'mr' }, gms[gid][note.author.id] && gms[gid][note.author.id].access_level >= 30)
-		if (note.author.id === mr.author.id ||
-			!gms[gid][note.author.id] ||
-			gms[gid][note.author.id].access_level < 30) return;
-		log.trace({
-type:'mr',
-result: result
-		});
-		log.trace({
-type:'mr',
-			author: note.author,
-			isAssignee: mr.assignee && note.author.id === mr.assignee.id,
-			yea: !!note.body.match(/\bYEA\b/)
-		});
-		result.push({
-			author: note.author,
-			isAssignee: mr.assignee && note.author.id === mr.assignee.id,
-			yea: !!note.body.match(/\bYEA\b/)
-		});
-		return result;
-	}, []);
+	return mr.notes.map(function (note) {
+		log.trace({ type:'isAuthor', result: note.author.id === mr.author.id});
+		log.trace({ type:'inGroup', result: !gms[gid][note.author.id]});
+		log.trace({ type:'accessLevel', result: gms[gid][note.author.id] && gms[gid][note.author.id].access_level});
+		if (!gms[gid][note.author.id] || gms[gid][note.author.id].access_level < 30) {
+			// not a group member or developer
+			return false;
+		} 
+		if (note.author.id === mr.author.id) {
+			if (note.body.match(/Added \d+ new commit/)) {
+				note.isUpdate = true;
+				return note;
+			}
+			return false;
+		}
+		note.isAssignee = mr.assignee && note.author.id === mr.assignee.id;
+		note.yea = !!note.body.match(/\bYEA\b/);
+		return note;
+	}).filter(Boolean);
+}
+function getReviewResult(notes) {
+	var all = {};
+	var yeas = {};
+	var result = {
+		assigneeId: null,
+		assigneeName: null,
+		assigneeYea: false,
+		postInit: true,
+		postReset: false,
+	};
+	for (var note, i = -1; note = notes[++i]; ) {
+		if (note.isUpdate) {
+			all = {};
+			yeas = {};
+			result.assigneeYea = false;
+			result.updatedAt = (new Date).toISOString();
+			continue;
+		}
+		if (note.author.username === 'creditcloud') {
+			result.postInit = false;
+			if (result.updatedAt) result.postReset = true;
+		}
+		all[note.author.id] = note.author.name;
+		if (note.yea) {
+			yeas[note.author.id] = note.author.name;
+		} else {
+			delete yeas[note.author.id];
+		}
+		if (note.isAssignee) {
+			result.assigneeId = note.author.id;
+			result.assigneeName = note.author.name;
+			result.assigneeYea = note.yea;
+		}
+	}
+	return assign(result, {
+		votes: Object.keys(all).map(function (id) {
+			return {
+				id: id,
+				name: all[id],
+				yea: !!yeas[id]
+			};
+		}),
+	});
+}
+function postReviewNotes(mr) {
+	log.info({mr: mr}, 'posting review notes');
 }
 var ticking = false;
 var tick = co.wrap(function *() {
@@ -71,13 +114,20 @@ var tick = co.wrap(function *() {
 	var ps = JSON.parse(yield db.getAsync('tick:ps'));
 	var mrs = JSON.parse(yield db.getAsync('tick:mrs'));
 	mrs.forEach(function (mr) {
-		mr.reviews = getReviews(gms, ps, mr); 
+		if (!mr.notes.filter(function (note) {
+					return note.author.username === 'creditcloud';
+				}).length) {
+		}
+		mr.notes = augmentNotes(gms, ps, mr); 
+		mr.review = { updatedAt: mr.created_at };
+		assign(mr.review, getReviewResult(mr.notes));
+		postReviewNotes(mr);
 /*
 		mr.reviewAll = countYeas(gms, ps, mr, mr.notes, true);
 		mr.reviewYea = countYeas(gms, ps, mr, mr.notes);
 		mr.reviewYeaByAssignee = yeaByAssignee(mr, mr.notes);
 */
-		log.trace({type: 'mr', reviews: mr.reviews});
+		log.trace({notes: mr.notes, review: mr.review, mr: mr});
 	});
 	yield sleep(1000);
 	db.publish('tock', true);
